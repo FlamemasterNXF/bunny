ae1bffdf618ad6c5969967dac14075fad00d546d
Braeden Lotterman <flamemasternxf@gmail.com>
2025-01-15 20:04:24 -0500

    feat: Impl. Luck

--- src/bunny/lootbox.js
+++ src/bunny/lootbox.js
@@ -1,17 +1,34 @@
-function getLootboxRarity(){
+function weighProbabilities(luck){
+    const probabilities = rarityData.map(rarity => rarity.probability)
+    const mythicalProbability = probabilities[probabilities.length - 1] // Mythics are not effected by luck
+
+    const weightedProbabilities = probabilities.map((probability, index) => {
+        if(index === probabilities.length - 1) return mythicalProbability
+        return probability * (1 + luck * (index / (probabilities.length - 2)))
+    })
+
+    // I think this normalizes things... hopefully
+    const totalWeightedProbability = weightedProbabilities.reduce((sum, probability) => sum + probability, 0)
+    return weightedProbabilities.map(probability => probability / totalWeightedProbability)
+}
+
+function getLootboxRarity(luck = 0) {
+    const weightedRarityData = weighProbabilities(luck)
     const rand = Math.random()
     let cumulativeProbability = 0
 
     for (let i = 0; i < rarityData.length; i++) {
-        cumulativeProbability += rarityData[i].probability
+        cumulativeProbability += weightedRarityData[i]
         if (rand < cumulativeProbability) return i
     }
 
-    return alert(`SERIOUS ERROR IN openLootbox()\nrand:${rand}\ncProb:${cumulativeProbability}`)
+    console.error(`SERIOUS ERROR in getLootboxRarity(): rand:${rand}, cumulativeProbability:${cumulativeProbability}`)
+    return -1
 }
 
-function openLootbox(){
+function openLootbox(luck = 0){
     let rarity = getLootboxRarity()
+    if(rarity === -1) console.error(`SERIOUS ERROR in openLootbox(): luck:${luck}`)
     let bunnyID = getRandom(0, getBunniesByRarity(rarity).length)
 
     let stats = {
