0e65e24384a7a5f522750216852085236edd4602
Braeden Lotterman <flamemasternxf@gmail.com>
2025-01-22 18:20:16 -0500

    chore: Remove Reference to Paragon Level Boost

--- src/global/stats.js
+++ src/global/stats.js
@@ -15,6 +15,11 @@ let newStats = {
     }
 }
 
+// WIP ?
+function getBunnyStat(stat, paragonLevel){
+    return stat * (1 + paragonLevel / 10)
+}
+
 function getShieldedDamage(damage){
     return damage*(1-data.combat.currentBunny.data.stats.shield);
 }
